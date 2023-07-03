9d394ff7fe03d3fdc90fc67430b2024fff83f688
candela mena bisignano <candelamena007@gmail.com>
2023-07-02 23:47:54 -0300

    deliCake

--- src/components/TituloMain.js
+++ src/components/TituloMain.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const TituloMain = () => {
   return (
-    <div className='absolute'>
-      <h1>tu inspiracion</h1>
-      <h2>la encontras con nuestas delicias.</h2>
-      <button>ir a la tienda</button>
+    <div className='absolute top-[300px] left-[250px] text-left'>
+      <h1 className='text-[50px] text-white'>Tu inspiracion</h1>
+      <h2 className='text-[40px] pb-[20px] text-white'>la encontras con nuestas delicias.</h2>
+      <button className='pt-[10px] pb-[10px] pl-[15px] pr-[15px] border-pink-200 bg-white text-pink-200 border-[2px] rounded hover:scale-125 hover:ease-out duration-300 hover:text-white hover:border-none hover:bg-pink-200 '>ir a la tienda</button>
     </div>
   )
 }
